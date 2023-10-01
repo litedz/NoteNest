fbbf64894b63214d95360b552a40ea4998383781
litedz <maamarjoker@gmail.com>
2023-10-01 17:47:42 +0100

    wip

--- src/notenestServiceProvider.php
+++ src/notenestServiceProvider.php
@@ -31,7 +31,7 @@ class notenestServiceProvider extends ServiceProvider
     {
         AboutCommand::add('Note Nest', fn () => ['Version' => '1.0.0']);
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'notenest');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'notenest');
         $this->publishes([
             __DIR__ . '../config/notenest.php' => config_path('notenest.php'),
         ]);
