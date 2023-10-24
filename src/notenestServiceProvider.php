059d3fffa972e4124776de3d50207682d7d2d928
litedz <litedz@users.noreply.github.com>
2023-10-24 18:18:13 +0000

    Fix styling

--- src/notenestServiceProvider.php
+++ src/notenestServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace notenest\notenest;
 
-
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 use notenest\notenest\Livewire\Note;
@@ -17,22 +15,22 @@ class notenestServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'notenest');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'notenest');
 
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/livewire'),
+            __DIR__.'/../resources/views' => resource_path('views/livewire'),
         ], 'notenest-views');
         $this->publishes([
-            __DIR__ . '/../resources/js' => resource_path('js'),
+            __DIR__.'/../resources/js' => resource_path('js'),
         ], 'notenest-js');
 
         $this->publishes([
-            __DIR__ . '/../public' => public_path('notenest'),
+            __DIR__.'/../public' => public_path('notenest'),
         ], 'notenest-public');
 
         $this->publishes([
-            __DIR__ . '/../database/migrations/' => database_path('migrations'),
+            __DIR__.'/../database/migrations/' => database_path('migrations'),
         ], 'notenest-migrations');
 
         Livewire::component('Note', Note::class);
